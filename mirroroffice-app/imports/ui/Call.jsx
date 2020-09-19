f76cf345efd1b28f65e209baaec3e9bedaf7d1d2
jonathanlehner <lehnerj@student.ethz.ch>
2020-09-19 22:19:06 +0200

    show emails

--- mirroroffice-app/imports/ui/Call.jsx
+++ mirroroffice-app/imports/ui/Call.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
-const User = Meteor.users;
+const Users = Meteor.users;
 
 class Call extends React.Component {
     constructor(){
@@ -23,7 +23,7 @@ class Call extends React.Component {
                             {
                             this.props.users && this.props.users.map(user => (
                                 <div key={user._id} onClick={() => this.callUser(user._id)}>
-                                    {user._id}
+                                    {user._id}: {user.emails && user.emails[0].address}
                                 </div>
                             ))
                             }
@@ -36,11 +36,12 @@ class Call extends React.Component {
 
 export default withTracker(()=>{
     const usersLoading = !Meteor.subscribe("all_users").ready();
-    const users = User.find({
+    const users = Users.find({
         _id:{
             $ne : Meteor.userId()
         }
     }).fetch();
+    console.log(users)
     return {
         usersLoading,
         users
